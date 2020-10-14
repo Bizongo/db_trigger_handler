b4da920a52fc91e397a1cc2ed9aac91e57b21799
Kumar <mayank.kumar@bizongo-admins-MacBook-Pro-4.local>
2020-10-15 00:01:06 +0530

    added data

--- lib/helpers/sql.rb
+++ lib/helpers/sql.rb
@@ -1,10 +1,12 @@
 module SQL
-  def self.subscribe_channel(connection, channel)
-    execute_query(connection, "LISTEN #{channel}")
-  end
+  class << self
+    def subscribe_channel(connection, channel)
+      execute_query(connection, "LISTEN #{channel}")
+    end
 
-  private
-  def execute_query(connection, query)
-    connection.execute(query)
+    private
+    def execute_query(connection, query)
+      connection.execute(query)
+    end
   end
 end

851423bc3a4e6b50f3ecd05f7f85bb642e169094
Kumar <mayank.kumar@bizongo-admins-MacBook-Pro-4.local>
2020-10-14 22:48:03 +0530

    fixed import

--- lib/db_trigger_handler.rb
+++ lib/db_trigger_handler.rb
@@ -1,13 +1,15 @@
 require "db_trigger_handler/version"
 
 module DbTriggerHandler
-  def self.included(base)
-    Rails.logger.info "Base :- #{base}"
-    Rails.logger.info "Base :- #{notification_channels}"
-  end
+  class << self
+    def init
+      Rails.logger.info "Base :- #{base}"
+      Rails.logger.info "Base :- #{notification_channels}"
+    end
 
-  private
-  def notification_channels
-    %w[SHIPMENT_CREATED SHIPMENT_CHANGED DPIR_CHANGED]
+    private
+    def notification_channels
+      %w[SHIPMENT_CREATED SHIPMENT_CHANGED DPIR_CHANGED]
+    end
   end
 end

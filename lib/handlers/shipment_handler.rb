d1884efd3c0618e30ae9aff2e8ad394650bac6c2
Kumar <mayank.kumar@bizongo-admins-MacBook-Pro-4.local>
2020-10-16 02:56:41 +0530

    added data

--- lib/handlers/shipment_handler.rb
+++ lib/handlers/shipment_handler.rb
@@ -1,8 +1,13 @@
+require 'helpers/sql'
+
 module ShipmentHandler
+  include SQL
+
   class << self
     def shipment_create_handler(connection, data)
       parsed_data = JSON.parse data
-      pp "Message :- #{parsed_data[:id]}, #{parsed_data['id']}"
+      shipment_create_data = SQL.get_all_shipment_info(connection, parsed_data['id'])
+      pp shipment_create_data
     end
   end
 end
--- lib/helpers/sql.rb
+++ lib/helpers/sql.rb
@@ -8,6 +8,31 @@ module SQL
       execute_query(connection, "UNLISTEN #{channel}")
     end
 
+    def get_all_shipment_info(connection, id)
+      shipment = get_shipment(connection,id)
+      {
+          shipment: shipment,
+          dispatch_plan: get_dispatch_plan(connection, shipment['dispatch_plan_id']),
+          dispatch_plan_item_relations: get_dispatch_plan_item_relations(connection, shipment['dispatch_plan_id'])
+      }
+    end
+
+    def get_shipment(connection, id)
+      execute_query(connection,
+                    "select * from supply_chain.shipments where id = #{id}").first
+    end
+
+    def get_dispatch_plan(connection, id)
+      execute_query(connection,
+                    "select * from supply_chain.dispatch_plans where id = #{id}").first
+    end
+
+    def get_dispatch_plan_item_relations(connection, dispatch_plan_id)
+      execute_query(connection,
+                    "select * from supply_chain.dispatch_plan_item_relations"+
+                        " where dispatch_plan_id = #{dispatch_plan_id}").first
+    end
+
     private
     def execute_query(connection, query)
       connection.execute(query)

8847f437cea4d7ae31083c325d400265d30c66b4
Kumar <mayank.kumar@bizongo-admins-MacBook-Pro-4.local>
2020-10-26 02:09:32 +0530

    removed error

--- lib/helpers/sql.rb
+++ lib/helpers/sql.rb
@@ -45,7 +45,7 @@ module SQL
     def get_shipment_from_dp(connection, dispatch_plan_id)
       execute_query(connection,
                     "select * from supply_chain.shipments"+
-                        " dispatch_plan_id = #{dispatch_plan_id}").to_a
+                        " where dispatch_plan_id = #{dispatch_plan_id}").to_a
     end
 
     def get_dispatch_plan(connection, id)

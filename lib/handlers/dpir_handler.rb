bc858eb20e971e1235412726829ad7bdb5fc6cff
Kumar <mayank.kumar@bizongo-admins-MacBook-Pro-4.local>
2020-10-26 03:20:58 +0530

    nv

--- lib/handlers/dpir_handler.rb
+++ lib/handlers/dpir_handler.rb
@@ -36,11 +36,11 @@ module DpirHandler
       quantity = dpir['shipped_quantity']
       case @type
       when 'SHIPPED_QUANTITY_CHANGE'
-        quantity = Math.abs(dpir['shipped_quantity']-old)
+        quantity = Math.abs(dpir['shipped_quantity'].to_f-old.to_f)
       when 'RETURNED_QUANTITY_CHANGE'
-        quantity = Math.abs(dpir['returned_quantity']-old)
+        quantity = Math.abs(dpir['returned_quantity'].to_f-old.to_f)
       when 'LOST_QUANTITY_CHANGE'
-        quantity = Math.abs(dpir['lost_quantity']-old)
+        quantity = Math.abs(dpir['lost_quantity'].to_f-old.to_f)
       end
       amount_without_tax = quantity.to_f * price_per_unit.to_f
       @amount = quantity * price_per_unit * (1+(gst_percentage/100))

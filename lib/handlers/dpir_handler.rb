58c12526291634800b98ea6e30ab5fa9c7346b63
Kumar <mayank.kumar@bizongo-admins-MacBook-Pro-4.local>
2020-10-26 17:10:28 +0530

    note data added

--- lib/handlers/dpir_handler.rb
+++ lib/handlers/dpir_handler.rb
@@ -13,6 +13,7 @@ module DpirHandler
       dpir_update_data = SQL.get_all_dpir_info(connection, parsed_data['id'])
       if [0,2].include? dpir_update_data[:dispatch_plan]['dispatch_mode']
         @note_type = 'CREDIT_NOTE'
+        @note_sub_type = ''
         @type = parsed_data['type']
         common_data = InvoiceCreationHelper.common_create_invoice_data dpir_update_data
         creation_data = add_information(dpir_update_data, common_data, parsed_data['old'])
@@ -27,8 +28,9 @@ module DpirHandler
       common_data.merge!({
         line_item_details: get_line_item_details(data[:dispatch_plan_item_relation], old),
         amount: @amount,
-        type: @note_type,
-        supporting_document_details: get_supporting_document_details(data)
+        type: @note_type + @note_sub_type,
+        supporting_document_details: get_supporting_document_details(data),
+        invoice_id_for_note: data[:shipment]['buyer_invoice_id']
       })
     end
 
@@ -36,18 +38,30 @@ module DpirHandler
       product_details = JSON.parse dpir['product_details']
       price_per_unit = product_details['order_price_per_unit']
       gst_percentage = product_details['order_item_gst']
-      quantity = dpir['shipped_quantity']
+      quantity =
+          dpir['shipped_quantity'].to_f - dpir['returned_quantity'].to_f - dpir['lost_quantity'].to_f
+      ppu_difference = 0
+      gst_difference = 0
       case @type
       when 'SHIPPED_QUANTITY_CHANGE'
         quantity = (dpir['shipped_quantity'].to_f-old.to_f).abs
+        get_note_type(dpir['shipped_quantity'], old)
       when 'RETURNED_QUANTITY_CHANGE'
         quantity = (dpir['returned_quantity'].to_f-old.to_f).abs
+        get_note_type(old, dpir['returned_quantity'])
       when 'LOST_QUANTITY_CHANGE'
         quantity = (dpir['lost_quantity'].to_f-old.to_f).abs
+        get_note_type(old, dpir['lost_quantity'])
       when 'PRICE_PER_UNIT_CHANGE'
         price_per_unit = (price_per_unit.to_f-old.to_f).abs
+        ppu_difference = price_per_unit
+        get_note_type(price_per_unit, old)
+        @note_sub_type = 'RATE_CHANGE_SYSTEM'
       when 'GST_CHANGE'
         gst_percentage = (gst_percentage.to_f-old.to_f).abs
+        gst_difference = gst_difference
+        get_note_type(gst_percentage, old)
+        @note_sub_type = 'TAX_CHANGE_SYSTEM'
       end
       amount_without_tax = quantity.to_f * price_per_unit.to_f
       @amount = quantity * price_per_unit * (1+(gst_percentage/100))
@@ -57,9 +71,19 @@ module DpirHandler
           dispatch_plan_item_relation_id: dpir['id'],
           quantity: quantity,
           price_per_unit: price_per_unit,
-          gst_percentage: gst_percentage,
-          amount_without_tax: amount_without_tax
+          tax_percentage: gst_percentage,
+          amount_without_tax: amount_without_tax,
+          ppu_difference: ppu_difference,
+          gst_difference: gst_difference,
       }]
+    end
+
+    def get_note_type(new, old)
+      if new.to_f < old.to_f
+        @note_type = 'CREDIT_NOTE'
+      else
+        @note_type = 'DEBIT_NOTE'
+      end
     end
 
     def get_if_igst_required data

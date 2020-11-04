8c4594ccf9602ffbb61ae7c2437bf5c4deddca62
Kumar <mayank.kumar@bizongo-admins-MacBook-Pro-4.local>
2020-11-05 02:14:20 +0530

    added data

--- lib/db_trigger_handler.rb
+++ lib/db_trigger_handler.rb
@@ -76,7 +76,7 @@ module DbTriggerHandler
     end
 
     def notification_channels
-      %w[shipment_created shipment_dpir_changed shipment_updated dpir_updated]
+      %w[shipment_created shipment_delivered shipment_dpir_changed shipment_updated dpir_updated]
     end
   end
 end

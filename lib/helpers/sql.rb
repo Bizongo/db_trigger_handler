b7b576fac9072da06a3d3bb933c6b8461b22937f
Kumar <mayank.kumar@bizongo-admins-MacBook-Pro-4.local>
2020-11-05 15:37:41 +0530

    delivered shipment POD

--- lib/helpers/sql.rb
+++ lib/helpers/sql.rb
@@ -68,7 +68,7 @@ module SQL
 
     def get_dispatch_plan_item_relations_unchecked(connection, dispatch_plan_id)
       execute_query(connection,
-                    "select * from supply_chain.dispatch_plan_item_relations dispatch_plan_id = #{dispatch_plan_id}").to_a
+                    "select * from supply_chain.dispatch_plan_item_relations where dispatch_plan_id = #{dispatch_plan_id}").to_a
     end
 
     def get_lost_dispatch_plan_item_relations(connection, dispatch_plan_id)

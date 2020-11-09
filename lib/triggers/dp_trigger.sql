57525aa2f6010d22cfd6082a0fbac90061430fdc
Kumar <mayank.kumar@bizongo-admins-MacBook-Pro-4.local>
2020-11-10 01:13:20 +0530

    nv

--- lib/triggers/dp_trigger.sql
+++ lib/triggers/dp_trigger.sql
@@ -8,7 +8,7 @@ DECLARE
 BEGIN
     IF TG_OP ilike('UPDATE')
     THEN
-        IF OLD.buyer_company_snapshot is not null and OLD.buyer_company_snapshot <> '{}'::jsonb and NEW.buyer_company_snapshot <> '{}'::jsonb
+        IF OLD.buyer_company_snapshot is not null and OLD.buyer_company_snapshot <> '{}'::jsonb and NEW.buyer_company_snapshot <> '{}'::jsonb and OLD.buyer_company_snapshot is distinct from NEW.buyer_company_snapshot
         THEN
             IF OLD.buyer_company_snapshot->>'billing_address' is not NULL and OLD.buyer_company_snapshot->>'billing_address' is DISTINCT FROM NEW.buyer_company_snapshot->>'billing_address'
             THEN
@@ -17,7 +17,7 @@ BEGIN
                 PERFORM pg_notify(channel, notify_data::text);
             END IF;
             RETURN NEW;
-        ELSIF OLD.destination_address_snapshot is not null and OLD.destination_address_snapshot <> '{}'::jsonb and NEW.destination_address_snapshot <> '{}'::jsonb
+        ELSIF OLD.destination_address_snapshot is not null and OLD.destination_address_snapshot <> '{}'::jsonb and NEW.destination_address_snapshot <> '{}'::jsonb and OLD.destination_address_snapshot is distinct from NEW.destination_address_snapshot
         THEN
             channel := 'dp_updated';
             notify_data := json_build_object('id', NEW.id, 'type', 'DESTINATION_CHANGE');
